3ecc9e2cd2a98fd12b3a185386ad4e481fbb5f40
Diend2023 <2577569250@qq.com>
2026-01-26 00:25:57 +0800

    update community and showcase

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -7,17 +7,131 @@ import FullWidthCard from '../../components/general/FullWidthCard';
 import Link from '@docusaurus/Link';
 import styles from './index.module.css';
 
-const TITLE = 'AIR Community';
+const TITLE = '幻想纹章 社区';
 const DESCRIPTION =
-  'AIR has been around for many years so you will find content in many places. Here we have listed the best ways to get involved in the AIR community as well as resources to answer your questions.';
+  '幻想纹章已经存在多年，所以你会在很多地方找到相关内容。这里我们列出了幻想纹章的各种社区。';
 
 function Community() {
   return (
     <Layout title={TITLE} description={DESCRIPTION}>
       <CommunityHeader title={TITLE} description={DESCRIPTION} />
       <main className="container margin-vert--lg">
-        {/* GITHUB - Discussions */}
+        {/* 网站 - 卓越逻辑 */}
         <FullWidthCard
+          left={
+            <div>
+              <div>
+                <img src="/images/tags/web.png" />
+              </div>
+              <h3>卓越逻辑</h3>
+              <p>
+                卓越逻辑【zygameui.cn】，幻想纹章唯一指定官方网站。
+              </p>
+              <div>
+                <Link
+                  className="button button--primary button--md"
+                  to="https://zygameui.cn/"
+                >
+                  卓越逻辑 &#10132;
+                </Link>
+              </div>
+            </div>
+          }
+          right={
+            <div className={styles.screenshot}>
+              <img src="/images/zygameui.cn_screen.png" />
+            </div>
+          }
+        />
+
+        {/* 网站 - 幻想纹章Club */}
+        <FullWidthCard
+          left={
+            <div>
+              <div>
+                <img src="/images/tags/web.png" />
+              </div>
+              <h3>幻想纹章Club</h3>
+              <p>
+                幻想纹章Club【hxwz3.cn】，来源于幻想纹章3交流群（其实就是本站咯）。
+              </p>
+              <div>
+                <Link
+                  className="button button--primary button--md"
+                  to="https://hxwz3.cn/"
+                >
+                  幻想纹章Club &#10132;
+                </Link>
+              </div>
+            </div>
+          }
+          right={
+            <div className={styles.screenshot}>
+              <img src="/images/hxwz3.cn_screen.png" />
+            </div>
+          }
+        />
+
+        {/* 网站 - 幻纹笔记 */}
+        <FullWidthCard
+          left={
+            <div>
+              <div>
+                <img src="/images/tags/web.png" />
+              </div>
+              <h3>幻纹笔记</h3>
+              <p>
+                幻纹笔记【hxwz2.cn】，是灵风创建的幻想纹章2及幻想纹章Next网站，可以登录注册并且发布、下载角色包。
+              </p>
+              <div>
+                <Link
+                  className="button button--primary button--md"
+                  to="https://hxwz2.cn/"
+                >
+                  幻纹笔记 &#10132;
+                </Link>
+              </div>
+            </div>
+          }
+          right={
+            <div className={styles.screenshot}>
+              <img src="/images/hxwz2.cn_screen.png" />
+            </div>
+          }
+        />
+
+        {/* 百度贴吧 - 幻想纹章吧 */}
+        <FullWidthCard
+          left={
+            <div>
+              <div>
+                <img src="/images/baidutieba_logo.png" />
+              </div>
+              <h3>幻想纹章吧</h3>
+              <p>
+                幻想纹章吧，是幻想纹章系列官方游戏综合贴吧。
+              </p>
+              <div>
+                <Link
+                  className="button button--primary button--md"
+                  to="https://tieba.baidu.com/f?kw=幻想纹章"
+                >
+                  幻想纹章吧 &#10132;
+                </Link>
+              </div>
+            </div>
+          }
+          right={
+            <div className={styles.screenshot}>
+              <img src="/images/hxwzba_screen.png" />
+            </div>
+          }
+        />
+
+        
+
+        {/* GITHUB - Discussions */}
+        {/* <FullWidthCard
           left={
             <div>
               <div>
@@ -44,10 +158,10 @@ function Community() {
               <img src="/images/github_discussions_screen.png" />
             </div>
           }
-        />
+        /> */}
 
         {/* GITHUB - Issues */}
-        <FullWidthCard
+        {/* <FullWidthCard
           left={
             <div>
               <div>
@@ -73,10 +187,10 @@ function Community() {
               <img src="/images/github_issuetracker_screen.png" />
             </div>
           }
-        />
+        /> */}
 
         {/* STARLING FORUM */}
-        <FullWidthCard
+        {/* <FullWidthCard
           left={
             <div>
               <div>
@@ -103,10 +217,10 @@ function Community() {
               <img src="/images/starling_forum_screen.png" />
             </div>
           }
-        />
+        /> */}
         
         {/* DISCORD */}
-        <FullWidthCard
+        {/* <FullWidthCard
           left={
             <div>
               <div>
@@ -131,7 +245,7 @@ function Community() {
               <img src="/images/discord_screen.png" />
             </div>
           }
-        />
+        /> */}
 
       </main>
     </Layout>
